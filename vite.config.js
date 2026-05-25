e8b0e94147584013a368f45f2c923612a2e708ac
productname <info@rocket.name>
2026-05-26 01:38:24 +0300

    Replace section divider with SVG image

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,45 @@ function inlineCssPlugin() {
   };
 }
 
+function inlineSvgPlugin() {
+  const replaceImgWithSvg = (html) => {
+    return html.replace(
+      /<img src="\/images\/([^"]+\.svg)"([^>]*)>/gi,
+      (match, filename, attrs) => {
+        const fs = require('fs');
+        const path = require('path');
+        const svgPath = path.resolve(__dirname, 'public/images', filename);
+
+        if (fs.existsSync(svgPath)) {
+          const svgContent = fs.readFileSync(svgPath, 'utf-8');
+          const svgMatch = svgContent.match(/<svg([^>]*viewBox="[^"]*"[^>]*)>/i);
+          const svgAttrs = svgMatch ? svgMatch[1] : ' viewBox="0 0 506.99 20.25"';
+          const innerContent = svgContent.match(/<svg[^>]*>([\s\S]*?)<\/svg>/i)?.[1] || svgContent;
+          const imgClass = attrs.match(/class="([^"]+)"/)?.[1] || '';
+          const svgClass = imgClass ? ` class="${imgClass}"` : '';
+          return `<svg${svgAttrs}${svgClass} xmlns="http://www.w3.org/2000/svg">${innerContent}</svg>`;
+        }
+        return match;
+      }
+    );
+  };
+
+  return {
+    name: 'inline-svg',
+    enforce: 'pre',
+    transformIndexHtml(html) {
+      return replaceImgWithSvg(html);
+    },
+    generateBundle(_, bundle) {
+      const htmlEntry = Object.entries(bundle).find(([name]) => name.endsWith('.html'));
+      if (!htmlEntry) return;
+
+      const [cssName, htmlChunk] = htmlEntry;
+      htmlChunk.source = replaceImgWithSvg(htmlChunk.source);
+    },
+  };
+}
+
 export default defineConfig({
   base: '/',
   plugins: [
@@ -33,5 +72,6 @@ export default defineConfig({
       png: { quality: 75 },
     }),
     inlineCssPlugin(),
+    inlineSvgPlugin(),
   ],
 });
